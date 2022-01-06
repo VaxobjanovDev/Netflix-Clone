79a4076096fa27cc5a37a43a2bcb20d6eb3701db
VaxobjanovDev <vaxobjanovdev@gmail.com>
2022-01-06 19:37:50 +0500

    Added some chnages

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Features from '../../component/Features/Features'
 import Navbar from '../../component/Navbar/Navbar'
+import List from '../../component/List/List'
 import './Home.css'
 
 const Home = () => {
     return (
         <div className="home-section">
           <Navbar/>
-					<Features type="se"/>           
+					<Features type="se"/>
+					<List/>  
+					<List/>           
         </div>
     )
 }
