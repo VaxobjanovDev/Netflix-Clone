51f9eddb329bded746975ab3350d451c26354379
VaxobjanovDev <VaxobjanovDev@gmail.com>
2022-02-04 16:07:28 +0500

    Added some component and statement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Home from './pages/Home/Home'
+import Watch from './pages/Watch/Watch'
+import Register from './pages/Register/Register'
+import Login from './pages/Login/Login'
+
+
 
 const App = () => {
     return (
         <>
-            <Home/>   
+            <Login/>   
         </>
     )
 }
