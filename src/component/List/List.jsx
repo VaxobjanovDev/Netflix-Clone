844f942a6aefedd0778b65ce6ae0b4a6a652155e
VaxobjanovDev <VaxobjanovDev@gmail.com>
2022-02-04 16:00:38 +0500

    Added List Component

--- src/component/List/List.jsx
+++ src/component/List/List.jsx
@@ -1,55 +1,57 @@
-import { ArrowBackIosOutlined } from '@mui/icons-material'
-import ListItem from './ListItem/ListItem'
-import './List.css'
-import { useRef } from 'react'
+import { ArrowBackIosOutlined } from "@mui/icons-material";
+import ListItem from "./ListItem/ListItem";
+import "./List.css";
+import { useRef, useState } from "react";
 
 const List = () => {
+  const [isMoved, setIsMoved] = useState(false);
+  const [silederNumber, setSliderNumber] = useState(0);
 
-	let listRef = useRef(null)
+  let listRef = useRef(null);
 
-	const handleClick = (side)=>{
-		let distance = listRef.current.getBoundingClientRect().x-50
-		if(side === "left"){
-			listRef.current.style.transform = `translateX(${230 + distance}px)`
-		}
-		if(side === "right"){
-			listRef.current.style.transform = `translateX(${-230 + distance}px)`
-		}
-	}
+  const handleClick = (side) => {			
+		setIsMoved(true)
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (side === "left" && silederNumber > 0) {
 
-    return (
-        <div className='list'>
-          <span className="listTitle">Continue to watch</span>
-					<div className="wrapper">
-						<ArrowBackIosOutlined onClick={()=>handleClick("left")} className='sliderArrow left'/>
-						<div ref={listRef} className="container">
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-							<ListItem/>
-						</div>
-						<ArrowBackIosOutlined onClick={()=>handleClick("right")} className='sliderArrow right'/>
-					</div>
+      setSliderNumber(silederNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (side === "right" && silederNumber < 5) {
+      setSliderNumber(silederNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
+
+  return (
+    <div className="list">
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosOutlined
+          onClick={() => handleClick("left")}
+          className="sliderArrow left"
+					style={{display:!isMoved && "none"}}
+        />
+        <div ref={listRef} className="container">
+          <ListItem index={0} />
+          <ListItem index={1} />
+          <ListItem index={2} />
+          <ListItem index={3} />
+          <ListItem index={4} />
+          <ListItem index={5} />
+          <ListItem index={6} />
+          <ListItem index={7} />
+          <ListItem index={8} />
+          <ListItem index={9} />  
+          <ListItem index={10} />   
         </div>
-    )
-}
+        <ArrowBackIosOutlined
+          onClick={() => handleClick("right")}
+          className="sliderArrow right"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default List
+export default List;
