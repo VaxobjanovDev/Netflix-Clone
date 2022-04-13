b4bfa6410d8be4f3f950e9f15f9b56dc15b4e7d6
VaxobjanovDev <VaxobjanovDev@gmail.com>
2022-04-13 21:15:04 +0500

    Adding components and getting list

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
-import Features from '../../component/Features/Features'
-import Navbar from '../../component/Navbar/Navbar'
-import List from '../../component/List/List'
-import './Home.css'
+import React, { useEffect, useState } from "react";
+import Features from "../../component/Features/Features";
+import Navbar from "../../component/Navbar/Navbar";
+import List from "../../component/List/List";
+import axios from "axios";
+import "./Home.css";
 
-const Home = () => {
-    return (
-        <div className="home-section">
-          <Navbar/>
-					<Features type="se"/>
-					<List/>  
-					<List/>           
-        </div>
-    )
-}
+const Home = ({ type }) => {
+  const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
 
-export default Home
+  useEffect(() => {
+    const getRandomList = async () => {
+      try {
+        const res = await axios.get(
+          `lists${type ? "?type=" + type : ""}${
+            genre ? "&genre=" + genre : ""
+          }`,
+          {
+            headers: {
+              token: "Bearer " + JSON.parse(localStorage.getItem("user")).accesToken,
+            },
+          }
+        );
+        console.log(res.data);
+        setLists(res.data)
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomList();
+  }, [type, genre]);
+  return (
+    <div className="home-section">
+      <Navbar />
+      <Features type={type} setGenre={setGenre}/>
+      {lists.map((list)=>(
+        <List key={list._id} list={list}/>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
