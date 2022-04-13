246c3fd14972fe4132fd9a8567ed63655c7741c3
VaxobjanovDev <VaxobjanovDev@gmail.com>
2022-04-13 21:17:55 +0500

    Getting data from back end

--- src/component/Features/Features.jsx
+++ src/component/Features/Features.jsx
@@ -1,50 +1,71 @@
-import { InfoOutlined, PlayArrow } from '@mui/icons-material'
-import './Features.css'
+import { InfoOutlined, PlayArrow } from "@mui/icons-material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./Features.css";
 
-const Features = ({type}) => {
-    return (
-        <div className='features'>
-					{type && (
-						<div className="category">
-							<span>{type === "movies"?"Movies":"Series"}</span>
-							<select name="genre" id="genre">
-								<option>Genre</option>
-								<option value="adventure">Adventure</option>
-								<option value="Comedy">Comedy</option>
-								<option value="Crime">Crime</option>
-								<option value="Fantasy">Fantasy</option>
-								<option value="Historical">Historical</option>
-								<option value="Horror">Horror</option>
-								<option value="Romance">Romance</option>
-								<option value="Sci-fi">Sci-fi</option>
-								<option value="Thriller">Thriller</option>
-								<option value="Western">Western</option>
-								<option value="Animation">Animation</option>
-								<option value="Drama">Drama</option>
-								<option value="Documentary">Documentary</option>
-							</select>
-						</div>
-					)}
-				 <img src="ad" alt="home-image" />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
-					<div className="info">
-						<img src="ad" alt="item-image" />
-						<p className="desc">
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci commodi dolor impedit ab doloremque rem, possimus perferendis, mollitia officiis ipsum aliquam incidunt ratione, iure quaerat ad at itaque sequi dolorem.
-						</p>
-						<div className="buttons">
-							<button className="play">
-								<PlayArrow/>
-								<span>Play</span>
-							</button>
-							<button className="more">
-								<InfoOutlined/>
-								<span>Info</span>
-							</button>
-						</div>
-					</div>
+const Features = ({ type, setGenre }) => {
+  const [content, setContent] = useState({});
+
+  useEffect(() => {
+    const getRandomContent = async () => {
+      try {
+        const res = await axios.get(`/movies/random?type=${type}`, {
+          headers: {
+            token: "Bearer " + JSON.parse(localStorage.getItem("user")).accesToken,
+          },
+        });
+        console.log(res.data);
+        setContent(res.data[0]);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomContent();
+  }, [type]);
+  return (
+    <div className="features">
+      {type && (
+        <div className="category">
+          <span>{type === "movies" ? "Movies" : "Series"}</span>
+          <select
+            name="genre"
+            id="genre"
+            onChange={(e) => setGenre(e.target.value)}
+          >
+            <option>Genre</option>
+            <option value="Adventure">Adventure</option>
+            <option value="Comedy">Comedy</option>
+            <option value="Crime">Crime</option>
+            <option value="Fantasy">Fantasy</option>
+            <option value="Historical">Historical</option>
+            <option value="Horror">Horror</option>
+            <option value="Romance">Romance</option>
+            <option value="Sci-fi">Sci-fi</option>
+            <option value="Thriller">Thriller</option>
+            <option value="Western">Western</option>
+            <option value="Animation">Animation</option>
+            <option value="Drama">Drama</option>
+            <option value="Documentary">Documentary</option>
+          </select>
         </div>
-    )
-}
+      )}
+      <img src={content.img} alt="home-image" />
+      <div className="info">
+        <img src={content.imgTitle} alt="item-image" />
+        <p className="desc">{content.desc}</p>
+        <div className="buttons">
+          <button className="play">
+            <PlayArrow />
+            <span>Play</span>
+          </button>
+          <button className="more">
+            <InfoOutlined />
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Features
-
+export default Features;
