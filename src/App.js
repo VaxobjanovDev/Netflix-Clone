87b8f8489612fd1bbc8a894ea1a5badc71522603
VaxobjanovDev <VaxobjanovDev@gmail.com>
2022-04-22 22:04:01 +0500

    Added Route and Navigation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,66 @@
-import React from 'react'
-import Home from './pages/Home/Home'
-import Watch from './pages/Watch/Watch'
-import Register from './pages/Register/Register'
-import Login from './pages/Login/Login'
-
-
+import React, { useContext } from "react";
+import Home from "./pages/Home/Home";
+import Watch from "./pages/Watch/Watch";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthContext } from "./authcontext/Authcontext";
 
 const App = () => {
-    return (
-        <>
-            <Login/>   
-        </>
-    )
-}
+  const { user } = useContext(AuthContext);
+  console.log(user)
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              user ? (
+               <Home />
+              ) : (
+                <Routes>
+                  <Route path="/register" element={<Register />} />
+                </Routes>
+              )
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              !user ? (
+                <Register />
+              ) : (
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                </Routes>
+              )
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              !user ? (
+                <Login />
+              ) : (
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                </Routes>
+              )
+            }
+          />
+          {user && (
+            <>
+              <Route path="/movies" element={<Home type="movies" />} />
+              <Route path="/series" element={<Home type="series" />} />
+              <Route path="/watch" element={<Watch />} />
+            </>
+          )}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
