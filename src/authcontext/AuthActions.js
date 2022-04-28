2559dd0e6588733051f1430d1d82cd3af97a8c00
VaxobjanovDev <VaxobjanovDev@gmail.com>
2022-04-28 22:28:33 +0500

    Added Register actions

--- src/authcontext/AuthActions.js
+++ src/authcontext/AuthActions.js
@@ -1,3 +1,16 @@
+export const registerStart = () => ({
+  type: "REGISTER_START",
+});
+
+export const registerSucces = (user1) => ({
+  type: "REGISTER_SUCCES",
+	payload:user1
+});
+
+export const registerFailure = () => ({
+  type: "REGISTER_FAILURE",
+});
+
 export const loginStart = () => ({
   type: "LOGIN_START",
 });
