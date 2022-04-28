8719c93b2adb2777f175d41e2f2b1e47fde40299
VaxobjanovDev <VaxobjanovDev@gmail.com>
2022-04-28 22:26:47 +0500

    Fixes bugs with router

--- src/App.js
+++ src/App.js
@@ -3,62 +3,44 @@ import Home from "./pages/Home/Home";
 import Watch from "./pages/Watch/Watch";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { AuthContext } from "./authcontext/Authcontext";
 
 const App = () => {
   const { user } = useContext(AuthContext);
-  console.log(user)
+  console.log(user);
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              user ? (
-               <Home />
+          {user ? (
+            <Route path="/" element={<Home />} />
+          ) : (
+              <Route path="/" element={<Navigate replace to="/register"/>} />
+          )}
+          <Route path="/register">
+          {!user ? (
+               <Route index element={<Register />}/>
               ) : (
-                <Routes>
-                  <Route path="/register" element={<Register />} />
-                </Routes>
-              )
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              !user ? (
-                <Register />
+              <Route path="/register" element={<Navigate replace to="/"/>} />
+              )}
+          </Route>
+          <Route path="/login">
+          {!user ? (
+               <Route index element={<Login />}/>
               ) : (
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                </Routes>
-              )
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              !user ? (
-                <Login />
-              ) : (
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                </Routes>
-              )
-            }
-          />
+              <Route path="/login" element={<Navigate replace to="/"/>} />
+              )}
+          </Route>
           {user && (
             <>
-              <Route path="/movies" element={<Home type="movies" />} />
+              <Route path="/movies" element={<Home type="movie" />} />
               <Route path="/series" element={<Home type="series" />} />
               <Route path="/watch" element={<Watch />} />
             </>
           )}
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   );
 };
