1ba021cdf3d07884a6ab829e4c54edf0a2fa4c78
VaxobjanovDev <VaxobjanovDev@gmail.com>
2022-04-28 22:27:58 +0500

    Added RegisterAPI

--- src/authcontext/ApiCall.js
+++ src/authcontext/ApiCall.js
@@ -1,5 +1,15 @@
 import axios from "axios"
-import { loginFailure, loginStart, loginSucces } from "./AuthActions"
+import { loginFailure, loginStart, loginSucces, registerFailure, registerStart, registerSucces } from "./AuthActions"
+
+export const registerAPI = async(user1, dispatch)=>{
+	dispatch(registerStart())
+	  try{
+		  const res = await axios.post("auth/register", user1)
+		  dispatch(registerSucces(res.data))
+	  }catch(e){
+		  dispatch(registerFailure())
+	  }
+  }
 
 export const loginAPI = async(user, dispatch)=>{
   dispatch(loginStart())
