7f212964a01d8596c2028dd56316914a129a9cd3
VaxobjanovDev <VaxobjanovDev@gmail.com>
2022-04-28 22:30:24 +0500

    Added some changes

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,62 +1,87 @@
-import './Register.css'
+import "./Register.css";
 import { Logo } from "../../svg/AllSvg";
-import { useRef, useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-const Register = ()=>{
+import { useContext, useEffect, useRef, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../authcontext/Authcontext";
+import { registerAPI } from "../../authcontext/ApiCall";
+const Register = () => {
+  const navigate = useNavigate();
 
-	const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
 
-	const [email, setEmail] = useState("")
-	const [password, setPassword] = useState("")
-	const [username, setUsername] = useState("")
+  const { dispatch } = useContext(AuthContext);
 
+  const emailRef = useRef();
 
-	const emailRef = useRef()
-	const userRef = useRef()
-	const passRef = useRef()
+  useEffect(() => {}, [dispatch]);
+  const handleClick = () => {
+    setEmail(emailRef.current.value);
+  };
 
+  const handleClickPass = async () => {
+    try {
+      registerAPI({ username, email, password }, dispatch);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	const handleClick = ()=>{
-		setEmail(emailRef.current.value)
-	}
-
-	const handleClickPass = async(e)=>{
-		e.preventDefault()
-		setUsername(userRef.current.value)
-		setPassword(passRef.current.value)
-		try{
-			await axios.post("auth/register",{email,username,password})
-			navigate("/login")
-		}catch(err){
-			console.log(err)
-		}
-	}
-	
-    return(
-        <div className="register">
-          <div className="top">
-						<div className="wrapper">
-							<Logo width="150" height="100"/>
-							<button className="loginButton">Sign In</button>
-						</div>
-					</div>  
-					<div className="container">
-						<h1>Unlimited movies, TV shows, and more</h1>
-						<h2>Watch anywhere. Cancel anytime</h2>
-						<p>Ready to watch? Enter your email to create or restart your membership</p>
-						{!email?(<div className="input">
-						<input type="email" placeholder="email address" ref={emailRef}/>
-						<button onClick={handleClick} className="registerButton">Get Started</button>
-						</div>):(<form className="form">
-						<input type="text" placeholder="Username" ref={userRef}/>
-						<input type="password" placeholder="password" ref={passRef}/>
-						<button onClick={handleClickPass} className="registerButton">Start</button>
-						</form>)}
-					</div>
-					
+  return (
+    <div className="register">
+      <div className="top">
+        <div className="wrapper">
+          <Logo width="150" height="100" />
+          <Link to="/login">
+            <button className="loginButton">Sign In</button>
+          </Link>
         </div>
-    )
-}
+      </div>
+      <div className="container">
+        {!email ? (
+          <div className="registration">
+            <div className="register-title">
+              <h1>Unlimited movies, TV shows, and more</h1>
+              <h2>Watch anywhere. Cancel anytime</h2>
+              <p>
+                Ready to watch? Enter your email to create or restart your
+                membership
+              </p>
+            </div>
+            <div className="input">
+              <input type="email" placeholder="Email address" ref={emailRef} />
+              <button onClick={handleClick} className="registerButton">
+                Get Started
+              </button>
+            </div>
+          </div>
+        ) : (
+          <form className="form">
+			  <div className="title">
+				  <h3>Enter Username and Password.</h3>
+			  </div>
+            <input
+				id="username"
+              type="text"
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleClickPass} className="start-button">
+              Start
+            </button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
