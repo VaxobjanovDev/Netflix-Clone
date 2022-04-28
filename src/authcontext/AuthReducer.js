21875bba7ed7846f3f112c5590ef6c797628f483
VaxobjanovDev <VaxobjanovDev@gmail.com>
2022-04-28 22:29:00 +0500

    Added Register reducer

--- src/authcontext/AuthReducer.js
+++ src/authcontext/AuthReducer.js
@@ -1,5 +1,23 @@
 const AuthReducer = (state, action) => {
   switch (action.type) {
+    case "REGISTER_START":
+      return {
+        user: null,
+        isFetching: true,
+        error: false,
+      };
+    case "REGISTER_SUCCES":
+      return {
+        user: action.payload,
+        isFetching: true,
+        error: false,
+      };
+    case "REGISTER_FAILURE":
+      return {
+        user: null,
+        isFetching: false,
+        error: true,
+      };
     case "LOGIN_START":
       return {
         user: null,
